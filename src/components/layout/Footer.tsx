8d233cba77687c46ed7b71cf0f0e99c50560f918
Monikakommana <monikakommana@gmail.com>
2026-04-01 11:39:53 +0530

    change the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer style={{ backgroundColor: "orange", textAlign: "center", color: "white", padding: "10px" }}>
+      
+      <div className="w-full grid grid-cols-12">
+        
+        {/* Column 1 */}
+        <div className="col-span-4">
+          <h2 style={{ fontStyle: "italic", color: "white" }}>Quick Links</h2>
+          <ul style={{ listStyle: "none", padding: 0, fontStyle: "italic", fontSize: "15px" }}>
+            <li>Home</li>
+            <li>My Profile</li>
+            <li>Benefits & Policies</li>
+            <li>Payroll & Compensation</li>
+            <li>Time & Attendance</li>
+          </ul>
+        </div>
 
-export default Footer
+        {/* Column 2 */}
+        <div className="col-span-4">
+          <h2 style={{ fontStyle: "italic", color: "white" }}>Resources</h2>
+          <ul style={{ listStyle: "none", padding: 0, fontStyle: "italic", fontSize: "15px" }}>
+            <li>HR Documents</li>
+            <li>Company Directory</li>
+            <li>Training & Development</li>
+            <li>IT Support</li>
+            <li>Wellness Programs</li>
+          </ul>
+        </div>
+
+        {/* Column 3 */}
+        <div className="col-span-4">
+          <h2 style={{ fontStyle: "italic", color: "white" }}>Contact Us</h2>
+          <ul style={{ listStyle: "none", padding: 0, fontStyle: "italic", fontSize: "15px" }}>
+            <li>Email: 50@gmail.com</li>
+            <li>Phone: 5050505050</li>
+            <li>Address: 123 Main Street</li>
+            <li>Pincode: 532421</li>
+          </ul>
+        </div>
+
+      </div>
+
+      {/* Bottom */}
+      <div style={{ textAlign: "center", marginTop: "20px", borderTop: "1px solid #334155", paddingTop: "10px", fontSize: "15px" }}>
+        <p>© 2026 Employee Portal. All rights reserved.</p>
+      </div>
+
+    </footer>
+  );
+};
+
+export default Footer;
